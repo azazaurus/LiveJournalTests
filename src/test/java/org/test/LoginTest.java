65620bdda0416b64da49bbc68802526b907c1314
azaliyas-github <1maxovmax1@gmail.com>
2021-11-03 19:30:27 +0300

    extract TestBase

--- src/test/java/org/test/LoginTest.java
+++ src/test/java/org/test/LoginTest.java
@@ -12,23 +12,7 @@ import java.util.concurrent.*;
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
 
-public class LoginTest {
-	private static Properties config;
-	private static WebDriver driver;
-	private static LoginPage loginPage;
-
-	@BeforeAll
-	public static void setUp() {
-		config = getConfig();
-		driver = initializeWebDriver();
-		loginPage = new LoginPage(driver);
-	}
-
-	@AfterAll
-	public static void tearDown() {
-		driver.quit();
-	}
-
+public class LoginTest extends TestBase {
 	@Test
 	public void loginsSuccessfully() {
 		assertThat(loginPage.isLoaded(), is(true));
@@ -38,33 +22,5 @@ public class LoginTest {
 		loginPage.login(userName, userPassword);
 
 		new WebDriverWait(driver, 3).until(driver -> !loginPage.isLoaded());
-	}
-
-	private static Properties getConfig() {
-		try (InputStream inputStream = LoginTest.class.getClassLoader().getResourceAsStream("config.properties")) {
-			Properties properties = new Properties();
-			properties.load(inputStream);
-			return properties;
-		} catch (IOException e) {
-			throw new IllegalStateException(e);
-		}
-	}
-
-	private static WebDriver initializeWebDriver() {
-		System.setProperty("webdriver.chrome.driver", config.getProperty("ChromeDriver.Path"));
-		WebDriver driver = new ChromeDriver();
-
-		driver.manage().window().maximize();
-		driver.get(config.getProperty("LoginPage.Url"));
-
-		return driver;
-	}
-
-	private static void wait(int seconds) {
-		try {
-			TimeUnit.SECONDS.sleep(seconds);
-		} catch (InterruptedException e) {
-			throw new IllegalStateException(e);
-		}
 	}
 }
