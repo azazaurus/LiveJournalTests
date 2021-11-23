579172582d984c77864ef9b6362a93890985a409
azaliyas-github <1maxovmax1@gmail.com>
2021-11-24 02:33:57 +0300

    Read generated test posts

--- src/test/java/org/test/data/Post.java
+++ src/test/java/org/test/data/Post.java
@@ -4,6 +4,12 @@ public class Post {
 	public final String title;
 	public final String text;
 
+	@SuppressWarnings("unused")
+	private Post() {
+		title = null;
+		text = null;
+	}
+
 	public Post(String title, String text) {
 		this.title = title;
 		this.text = text;
--- src/test/java/org/test/tests/TestAddNewPost.java
+++ src/test/java/org/test/tests/TestAddNewPost.java
@@ -1,16 +1,38 @@
 package org.test.tests;
 
-import org.junit.jupiter.api.*;
+import com.fasterxml.jackson.core.type.*;
+import com.fasterxml.jackson.databind.*;
+import org.junit.jupiter.params.*;
+import org.junit.jupiter.params.provider.*;
+import org.test.data.*;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
 
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
 
 public class TestAddNewPost extends LoggedInTestBase {
-	@Test
-	public void addsNewPostSuccessfully() {
+	@ParameterizedTest
+	@MethodSource("addsNewPostSuccessfully_testCaseSource")
+	public void addsNewPostSuccessfully(Post post) {
 		assertThat(app.site.isLoggedIn(), is(true));
-		app.site.addNewPost("New test post!", "If you see this - it`s working");
+		app.site.addNewPost(post.title, post.text);
 		app.navigation.goToMyPublishedPostsPage();
 		app.wait(5);
+	}
+
+	public static Stream<Arguments> addsNewPostSuccessfully_testCaseSource() {
+		var jsonSerializer = new ObjectMapper();
+		var postsJson = LoggedInTestBase.class.getClassLoader().getResourceAsStream("test-posts.json");
+		Collection<Post> posts;
+		try {
+			posts = jsonSerializer.readValue(postsJson, new TypeReference<>() { });
+		} catch (IOException e) {
+			throw new IllegalStateException(e);
+		}
+
+		return posts.stream().map(Arguments::of);
 	}
 }
