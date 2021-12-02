cc9ed9dfadcdaf605afa291576f1dbf6ae85aa56
azaliyas-github <1maxovmax1@gmail.com>
2021-12-02 04:35:20 +0300

    Test login with invalid credential

--- src/test/java/org/test/tests/LoginTest.java
+++ src/test/java/org/test/tests/LoginTest.java
@@ -3,23 +3,27 @@ package org.test.tests;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.support.ui.*;
 
-import static org.hamcrest.MatcherAssert.*;
-import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class LoginTest extends TestBase {
 	@Test
-	public void loginsSuccessfully() {
-		if (app.login.isLoggedIn())
-			return;
-
-		app.navigation.goToLoginPage();
-		assertThat(app.login.isLoaded(), is(true));
-
+	public void loginWithValidData() {
 		var userName = app.config.getProperty("User.Name");
 		var userPassword = app.config.getProperty("User.Password");
+
+		app.login.login(userName, userPassword);
+		new WebDriverWait(app.driver, 3).until(driver -> !app.login.isLoaded());
+
+		assertTrue(app.login.isLoggedIn(userName));
+	}
+
+	@Test
+	public void loginWithInvalidData() {
+		var userName = app.config.getProperty("InvalidUser.Name");
+		var userPassword = app.config.getProperty("InvalidUser.Password");
+
 		app.login.login(userName, userPassword);
 
-		new WebDriverWait(app.driver, 3).until(driver -> !app.login.isLoaded());
-		app.wait(2);
+		assertFalse(app.login.isLoggedIn());
 	}
 }
