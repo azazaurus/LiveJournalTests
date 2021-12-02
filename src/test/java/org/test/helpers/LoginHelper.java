cd1469aa6aa2dd81f24bcac3b7dec86a109658fc
azaliyas-github <1maxovmax1@gmail.com>
2021-12-02 04:12:20 +0300

    Add authorization methods

--- src/test/java/org/test/helpers/LoginHelper.java
+++ src/test/java/org/test/helpers/LoginHelper.java
@@ -4,6 +4,12 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.support.*;
 import org.test.*;
 
+import java.net.*;
+import java.nio.charset.*;
+import java.util.*;
+import java.util.regex.*;
+import java.util.stream.*;
+
 public class LoginHelper extends HelperBase {
 	public LoginHelper(ApplicationManager app) {
 		super(app);
@@ -36,6 +42,49 @@ public class LoginHelper extends HelperBase {
 		return app.driver.findElements(By.xpath("//*[contains(@class, 's-header-item__link--login')]")).isEmpty();
 	}
 
+	public boolean isLoggedIn(String userName) {
+		if (!isLoggedIn())
+			return false;
+
+		var logoutPageUrl = logoutButton.getAttribute("href");
+		var currentUserName = getUserNameFromLogoutPageUrl(logoutPageUrl);
+		return currentUserName.equalsIgnoreCase(userName);
+	}
+
+	public void logout() {
+		app.navigation.goToMainPage();
+		app.driver.get(logoutButton.getAttribute("href"));
+	}
+
+	private static String getUserNameFromLogoutPageUrl(String logoutUrl) {
+		URL parsedUrl;
+		try {
+			parsedUrl = new URL(logoutUrl);
+		} catch (MalformedURLException e) {
+			throw new IllegalStateException(e);
+		}
+
+		var queryStringParameters = parseQueryString(parsedUrl.getQuery());
+		return queryStringParameters.stream()
+			.filter(x -> x.getKey().equals("user"))
+			.findFirst()
+			.get().getValue();
+	}
+
+	private static List<Map.Entry<String, String>> parseQueryString(String queryString) {
+		return Pattern.compile("&")
+			.splitAsStream(queryString)
+			.map(s -> Arrays.copyOf(s.split("=", 2), 2))
+			.map(o -> Map.entry(decode(o[0]), decode(o[1])))
+			.collect(Collectors.toList());
+	}
+
+	private static String decode(final String encoded) {
+		return Optional.ofNullable(encoded)
+			.map(e -> URLDecoder.decode(e, StandardCharsets.UTF_8))
+			.orElse(null);
+	}
+
 	private static final String userNameInputXPath = "//input[@id='user']";
 
 	@FindBy(xpath = userNameInputXPath)
@@ -47,6 +96,6 @@ public class LoginHelper extends HelperBase {
 	@FindBy(xpath = userNameInputXPath + "/ancestor::form/*[@name='action:login']")
 	private WebElement loginButton;
 
-	@FindBy(xpath = "//span[@class='s-header-item-post--long']")
-	private WebElement addNewPostButton;
+	@FindBy(xpath = "//a[contains(@class, 's-header-sub-list-item__link--logout') and text()='Выйти']")
+	private WebElement logoutButton;
 }
